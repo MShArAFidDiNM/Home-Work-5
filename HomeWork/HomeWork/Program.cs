336c2bb3176b4d72a4018579b109930d8f781aed
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-01 09:53:13 +0500

    Task2

--- HomeWork/HomeWork/Program.cs
+++ HomeWork/HomeWork/Program.cs
@@ -10,6 +10,37 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+
+            Console.Write("Massiv uzunligini kiriting : ");
+            int num1 = int.Parse(Console.ReadLine());
+            int[] array = new int[num1];
+
+            for (int i = 0; i < num1; i++)
+            {
+                int k = rnd.Next(100);
+                Console.Write($"{k}, ");
+                array[i] = k;
+            }
+
+            Console.Write("K :");
+            int karrali = int.Parse(Console.ReadLine());
+
+            int g = 0;
+
+            for(int i = 0; i < num1; i++)
+            {
+                if (array[i] % karrali == 0)
+                {
+                    g++;
+                    Console.WriteLine($"Javob : {array[i]}");
+                }
+            }
+            if (g == 0)
+            {
+                Console.WriteLine("Natija aniqlanmadi qaytadan urinib ko'ring ");
+            }
+            Main(args);
         }
     }
 }
