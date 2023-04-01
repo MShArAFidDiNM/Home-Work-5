2b70b78a294cdcd09cb975ba9c9149a163eb88a5
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-01 10:44:02 +0500

    Task7

--- HomeWork/HomeWork/Program.cs
+++ HomeWork/HomeWork/Program.cs
@@ -10,6 +10,32 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+
+            Console.Write("Massiv uzunligini kiriting : ");
+            int num1 = int.Parse(Console.ReadLine());
+            int[] arr = new int[num1];
+
+            for (int i = 0; i < num1; i++)
+            {
+                int k = rnd.Next(100);
+                arr[i] = k;
+            }
+
+            Console.WriteLine("Tartiblanmagan massiv:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Array.Sort(arr); // Massivni o'sish tartibida saralab berish
+
+            Console.WriteLine("\nTartiblangan massiv:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadKey();
         }
     }
 }
