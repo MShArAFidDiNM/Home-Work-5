95713d96acd42ca0edcf04c6fcc04f3b33877626
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-01 09:34:11 +0500

    Task1

--- HomeWork/HomeWork/Program.cs
+++ HomeWork/HomeWork/Program.cs
@@ -10,6 +10,13 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
+            Console.Write("enter number : ");
+            int num1=int.Parse(Console.ReadLine());
+            for (int i = 1; i < num1*2; i+=2)
+            {
+                Console.Write($"{i}, ");
+            }
+            Main(args);
         }
     }
 }
